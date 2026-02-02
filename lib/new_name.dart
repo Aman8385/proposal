c23c4317499980e0eadb8f6abb61bd47af64d62f
DELL <amanagarwal12527@gmail.com>
2026-02-03 00:34:44 +0530

    'asds'

---
+++ lib/new_name.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class OkayFile extends StatefulWidget {
+  const OkayFile({super.key});
+
+  @override
+  State<OkayFile> createState() => _OkayFileState();
+}
+
+class _OkayFileState extends State<OkayFile> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
